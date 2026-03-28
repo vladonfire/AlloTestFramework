f5cb48c854d255b6623947dcaec898ed685e942e
vladonfire <vladmushscak@icloud.com>
2026-03-28 16:14:07 +0200

    Add third test for Allo search verification

--- src/test/java/AlloTest.java
+++ src/test/java/AlloTest.java
@@ -38,5 +38,32 @@ public class AlloTest {
 
 
     }
+    @Test
+    public void testAirPodsSearchAndDetails() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://allo.ua/");
+        WebElement alloLogo = driver.findElement(By.xpath("//a[@class='v-logo']"));
+        Assert.assertTrue(alloLogo.isDisplayed(), "Логотип Алло не відображається на сторінці");
+        WebElement searchInput = driver.findElement(By.xpath("//input[@class= 'search-form__input']"));
+        searchInput.sendKeys("AirPods 3");
+        WebElement searchButton = driver.findElement(By.xpath("//button[@class='search-form__submit-button']"));
+        searchButton.click();
+        sleep(3000);
+        WebElement firstProductTitleElement = driver.findElement(By.xpath("(//a[@class= 'product-card__title'])[1]"));
+        String firstProductTitleText = firstProductTitleElement.getText();
+        Assert.assertTrue(firstProductTitleText.contains("Навушники Apple AirPods Pro 3 "), "назва першого товару не містить 'AirPods 3'");
+        String expectedName = firstProductTitleText;
+        firstProductTitleElement.click();
+        WebElement productTitleOnPage = driver.findElement(By.xpath("//h1[@class='p-view__header-title']"));
+        String actualName = productTitleOnPage.getText();
+        Assert.assertEquals(actualName,expectedName,"ім'я товару на сторінці не збігається з назвою не іншій сторінці");
+
+
+
+
+
+
+    }
 
 }
